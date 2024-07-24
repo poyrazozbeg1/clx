406cb97cbeb5bad32cbccc4080269c21df04f227
Poyraz Özbeğ <75093882+poyrazozbeg1@users.noreply.github.com>
2024-07-24 18:51:28 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,15 @@
+# Banners
+# banner1.py'yi çalıştır ve 3 saniye göster
+python banner1.py
+sleep 1  # Ekranı temizlemeden önce 1 saniye bekle
+clear
+
+# banner2.py'yi çalıştır ve 2 saniye göster
+python banner2.py
+sleep 1  # Ekranı temizlemeden önce 1 saniye bekle
+clear
+cd prodoc
+
+# Python programını çalıştırma
+echo "Running the Python script..."
+python clx.py
